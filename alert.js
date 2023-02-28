ba40e131023fb7f9a91b85c654cca0c83b4d327a
Ahsan41 <119751735+Ahsan41@users.noreply.github.com>
2023-02-28 11:02:21 +0500

    push

---
+++ alert.js
@@ -0,0 +1,19 @@
+// write a script to greet your website visitor using js alert
+
+alert("hello its muhammad ahsan")
+
+//  question 2 
+// write a script to display a message on your screen
+
+alert("error! please ennter a valid password")
+
+
+//  question 3
+// write a script to display a message on your screen
+// page hint : use line break
+alert("welcome JS LAND  /n Happy Coding")
+
+//  question 4
+// write a script to display a message on your screen in this sequence
+alert("welcome JS LAND")
+alert("Happy Coding")
---
+++ variableforstrings.js
@@ -0,0 +1,68 @@
+// question 1
+// Declare a variable called username.
+
+var username="ahsan"
+// Question 2
+// . Declare a variable called myName & assign to it a string 
+// that represents your Full Name
+
+var myname="Muhammad Ahsan"
+
+// Quetion 3
+// 3. Write a script to 
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+
+var message = "Hello World"
+alert(message)
+
+// question 5
+// Write a script to display the following alert using one JS 
+// variable
+
+var pizza =("pizza\n" + "pizz\n" + "piz\n" +"piz\n" +"p\n")
+
+alert(pizza)
+
+// question 6
+// Fortune Teller: Why pay a fortuneteller when you can 
+// just program your fortune yourself?
+// Store the following into variables: number of children, 
+// partner’s name, geographic location, and job title.
+
+
+
+
+
+
+
+// Question 7
+// . Declare a variable called email & assign to it a string that 
+// represents your Email Address (e.g. abc@xyz.com). Show 
+// the below mentioned message in an alert box. (Hint: use 
+// string concatenation)
+
+var email = "ahsanazeem706@gmail.com"
+
+alert(`my email adress is ${email}`)
+
+
+
+
+
+
+// Question 8
+// Declare a variable called book & give it the value “A 
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+var book =("nowadays i am learning A smarter way to learn JavaScript")
+alert(book)
+
+
+
+
+// Question 10
+// Write a script to display this in browser through JS
+var write=("yay! i can html content through java script")
+document.write(write);
